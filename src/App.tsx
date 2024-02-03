8d48a15a55ba34f899a014de4593f041ae451e00
mmnsrti <mmnsrti.work@gmail.com>
2024-02-03 12:51:04 +0330

    added datefns function  and changed some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Calender from "./components/Calender";
 import { EventProvider } from "./context/Event";
 import "./App.css";
@@ -8,6 +9,22 @@ function App() {
       <Calender />
     </EventProvider>
   );
+=======
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Calender from './components/Calender'
+
+function App() {
+
+  return (
+    <>
+    <Calender/>
+     
+    </>
+  )
+>>>>>>> 3d9964b (added datefns function  and changed some styles)
 }
 
 export default App;
