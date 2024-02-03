3bbdfddc0fce175971ebb9670b4dcace6965ec74
mmnsrti <mmnsrti.work@gmail.com>
2024-02-03 13:12:51 +0330

    added functionality to today and next and perv button

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -8,6 +8,8 @@ import {
   isBefore,
   endOfDay,
   isToday,
+  subMonths,
+  addMonths,
 } from "date-fns";
 import { formatDate } from "../utils/formatDate";
 import { cc } from "../utils/cc";
@@ -27,12 +29,12 @@ const Calender = () => {
     <div>
       <div className="calendar">
         <div className="header">
-          <button className="btn">Today</button>
+          <button className="btn" onClick={()=>setSelectedMonth(new Date)}>Today</button>
           <div>
-            <button className="month-change-btn">&lt;</button>
-            <button className="month-change-btn">&gt;</button>
+            <button className="month-change-btn" onClick={()=>setSelectedMonth(m=>subMonths(m,1))}>&lt;</button>
+            <button className="month-change-btn" onClick={()=>setSelectedMonth(m=>addMonths(m,1))}>&gt;</button>
           </div>
-          <span className="month-title">June 2023</span>
+          <span className="month-title">{formatDate(selectedMonth,{month:'long',year:'numeric'})} </span>
         </div>
         <div className="days">
           {calenderDays.map((day, index) => (
