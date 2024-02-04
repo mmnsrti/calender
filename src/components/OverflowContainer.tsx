942b355a74778303038dbe5bc57e769bf09db08a
mmnsrti <mmnsrti@gmail.com>
2024-02-04 14:52:28 +0330

    the overflow is not working as expected but other things are almost done

---
+++ src/components/OverflowContainer.tsx
@@ -0,0 +1,64 @@
+import { ReactNode, useLayoutEffect, useRef, useState } from "react";
+
+type OverflowContainerProps<T> = {
+  items: T[];
+  renderItem: (item: T) => ReactNode;
+  renderOverflow: (overflowAmount: number) => ReactNode;
+  getKey: (item: T) => ReactNode;
+  className?: string;
+};
+const OverflowContainer = ({
+  items,
+  getKey,
+  renderItem,
+  renderOverflow,
+  className,
+}: OverflowContainerProps<T>) => {
+  const [overflowAmount, setOverflowAmount] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+  useLayoutEffect(() => {
+   if (containerRef.current == null ) return 
+
+    const observer = new ResizeObserver((entries) => {
+        const containerElement =entries[0]?.target
+        if (containerElement == null) return
+
+        const children =containerElement.querySelectorAll<HTMLElement >('[data-item]')
+        const overflowElement  =containerElement.querySelector<HTMLElement >('[data-overflow]')
+        if (overflowElement!=null ) {
+            overflowElement.style.display ='none'
+        }
+        children.forEach(child=>child.style.removeProperty('display'))
+        let amount =0
+        for (let i = children.length -1 ; i>=0 ; i--){
+            const child = children[i]
+            if (containerElement.scrollHeight <= containerElement.clientHeight){
+                break
+            }
+            amount = children.length-i
+            child.style.display='none'
+            overflowElement?.style.removeProperty('display')
+        }
+        setOverflowAmount(amount)
+    } );
+
+    observer.observe(containerRef.current);
+    return () => {
+        observer.disconnect();
+    }
+}, [items])
+  return (
+    <>
+      <div className={className} ref={containerRef}>
+        {items.map((item) => (
+          <div data-item key={getKey(item)}>
+            {renderItem(item)}
+          </div>
+        ))}
+      </div>
+      <div data-overflow>{renderOverflow(overflowAmount)}</div>
+    </>
+  );
+};
+
+export default OverflowContainer;
